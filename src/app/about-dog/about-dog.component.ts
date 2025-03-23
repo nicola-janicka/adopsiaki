319883a51bb40c4fed74cfde0f5b74e55c31e403
nicola-janicka <janicka.nicola@gmail.com>
2025-03-23 13:49:37 +0100

    added dymanic adoption form

--- src/app/about-dog/about-dog.component.ts
+++ src/app/about-dog/about-dog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   RouterOutlet,
   RouterLink,
@@ -6,6 +6,10 @@ import {
   Router,
 } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { FirebaseService } from '../../firebaseService';
+import { ActivatedRoute } from '@angular/router';
+import { Dog } from '../../dog';
+import { AdoptionForm } from '../../adoptionForm';
 
 @Component({
   selector: 'app-about-dog',
@@ -15,5 +19,24 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './about-dog.component.css',
 })
 export class AboutDogComponent {
-  constructor(private router: Router) {}
+  dog!: Dog;
+
+  constructor(
+    private router: Router,
+    private fs: FirebaseService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.fs.getDog(id ? id : '').then((dog: Dog) => {
+      this.dog = dog;
+      this.dog.setID(id ? id : '');
+    });
+  }
+
+  goToAdoptionPage(id: string) {
+    this.router.navigate(['adoption-form/' + id]);
+  }
 }
